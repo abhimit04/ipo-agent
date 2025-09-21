f35c64072e82efa320f2829297b259a3abc00754
abhimit04 <abhimit04@gmail.com>
2025-09-22 01:28:55 +0530

    Commit

--- pages/api/ipos.js
+++ pages/api/ipos.js
@@ -5,7 +5,7 @@ import fetch from "node-fetch";
 /** --- Scrape IPO Central --- **/
 async function scrapeIPOCentral() {
   try {
-    const response = await fetch("https://ipocentral.in/ipo/", {
+    const response = await fetch("https://ipocentral.in/ipo-2025/", {
       headers: { "User-Agent": "Mozilla/5.0" }
     });
     const html = await response.text();
